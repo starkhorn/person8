2669ed28fd2e5a13b424ddc7c34bab8346162b71
Tharathorn Inphen <t.earendel@gmail.com>
2013-02-03 15:54:41 +0700

    Init repo with Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Person8::Application.config.secret_token = '3e681cfe7604af7dd0cb859760927916a4f430c14fd096d575cdbf251c19a2131cd59aee4700aba93819c7c69864a4c54154c6062af511e01007e26465fb9fb7'
